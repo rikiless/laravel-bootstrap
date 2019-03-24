b30da1e1a8f227ea9561b5a306c54b2d9f7ef800
Richard <richardtekel@me.com>
2019-03-24 19:38:40 +0100

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,12 +15,74 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-3">
-                Menu
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Documentation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
             </div>
-            <div class="col-sm">
-                <h1>Hello World!</h1>
 
-                <hr>
+            <div class="col-sm">
+                <div class="card mb-4 shadow-sm">
+                    <h5 class="card-header">Bitbucket</h5>
+                    <div class="card-body">
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                        <a href="" class="btn btn-primary">Connect to Bitbucket</a>
+                    </div>
+                </div>
+
+                <div class="card mb-4 shadow-sm">
+                    <h5 class="card-header">GitHub</h5>
+                    <div class="card-body">
+                        <a href="" class="btn btn-primary">Refresh Token</a>
+                        <a href="" class="btn btn-secondary">Unlink</a>
+                    </div>
+                </div>
+
+                <div class="card mb-4 shadow-sm">
+                    <h5 class="card-header">Invoices</h5>
+
+                    <table class="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td class="text-right"><a href="" class="btn btn-primary">Download</a></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td class="text-right"><a href="" class="btn btn-primary">Download</a></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td class="text-right"><a href="" class="btn btn-primary">Download</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
