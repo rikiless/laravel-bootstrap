a2ac73547c6bffbce215a1ed78947eaf07f8bfc3
Richard <richardtekel@me.com>
2019-03-26 17:10:11 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,7 +83,71 @@
                             </tr>
                             </tbody>
                         </table>
-                    </div>                </div>
+                    </div>
+                </div>
+
+                <div class="card mb-4 shadow-sm">
+                    <h5 class="card-header">Horizontal Form</h5>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group row">
+                                <label for="inputEmail3" class="col-sm-3 col-form-label text-sm-right">Email</label>
+                                <div class="col-sm">
+                                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputPassword3" class="col-sm-3 col-form-label text-sm-right">Password</label>
+                                <div class="col-sm">
+                                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                                </div>
+                            </div>
+                            <fieldset class="form-group">
+                                <div class="row">
+                                    <legend class="col-form-label col-sm-3 pt-0 text-sm-right">Radios</legend>
+                                    <div class="col-sm">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                                            <label class="form-check-label" for="gridRadios1">
+                                                First radio
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                                            <label class="form-check-label" for="gridRadios2">
+                                                Second radio
+                                            </label>
+                                        </div>
+                                        <div class="form-check disabled">
+                                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+                                            <label class="form-check-label" for="gridRadios3">
+                                                Third disabled radio
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+                            <div class="form-group row">
+                                <div class="col-sm-3 text-sm-right">Checkbox</div>
+                                <div class="col-sm">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" id="gridCheck1">
+                                        <label class="form-check-label" for="gridCheck1">
+                                            Example checkbox
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row mb-0">
+                                <div class="offset-3 col-sm">
+                                    <button type="submit" class="btn btn-primary">Sign in</button>
+                                    <button type="button" class="btn btn-success">Success</button>
+                                    <button type="button" class="btn btn-danger">Danger</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
