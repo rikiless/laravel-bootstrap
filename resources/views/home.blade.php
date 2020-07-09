891374a89683041dfe263b35eba5f34aca920a8b
Richard Tekel <richardtekel@me.com>
2020-07-09 08:23:49 +0200

    Add spinning loader on cursor when axios processing requests

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,46 +10,47 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.2.3/trix.min.css">
 
 <div class="container" id="app">
-    <div class="row">
-        <div class="col-sm-2">
-            <ul class="nav nav-pills flex-column">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Documentation</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-
-        <div class="col-sm">
-            <div class="card mb-4 shadow-sm">
-                <h5 class="card-header">Bitbucket</h5>
-                <div class="card-body">
-                    <p class="card-text">
-                        With supporting text below as a natural lead-in to additional content.
-                    </p>
-
-                    <a href="" class="btn btn-primary">Connect to Bitbucket</a>
-                </div>
+    <div :class="{ 'vue-wait': isLoading }">
+        <div class="row">
+            <div class="col-sm-2">
+                <ul class="nav nav-pills flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Documentation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
             </div>
 
-            <div class="card mb-4 shadow-sm">
-                <h5 class="card-header">GitHub</h5>
-                <div class="card-body">
-                    <a href="" class="btn btn-primary">Refresh Token</a>
-                    <a href="" class="btn btn-secondary">Unlink</a>
-                </div>
-            </div>
+            <div class="col-sm">
+                <div class="card mb-4 shadow-sm">
+                    <h5 class="card-header">Bitbucket</h5>
+                    <div class="card-body">
+                        <p class="card-text">
+                            With supporting text below as a natural lead-in to additional content.
+                        </p>
 
-            <div class="card mb-4 shadow-sm">
-                <h5 class="card-header">Invoices</h5>
+                        <a href="" class="btn btn-primary">Connect to Bitbucket</a>
+                    </div>
+                </div>
+
+                <div class="card mb-4 shadow-sm">
+                    <h5 class="card-header">GitHub</h5>
+                    <div class="card-body">
+                        <a href="" class="btn btn-primary">Refresh Token</a>
+                        <a href="" class="btn btn-secondary">Unlink</a>
+                    </div>
+                </div>
+
+                <div class="card mb-4 shadow-sm">
+                    <h5 class="card-header">Invoices</h5>
                     <table class="table table-striped table-hover mb-0">
                         <thead class="border-light">
                         <tr>
@@ -80,33 +81,34 @@
                         </tr>
                         </tbody>
                     </table>
+                </div>
+
+                <x-card>
+                    @slot('title')
+                        Horizontal Form
+                    @endslot
+
+                    <form-components></form-components>
+                </x-card>
+
+                <x-card>
+                    @slot('title')
+                        Form with Built Validation
+                    @endslot
+
+                    <form-validation></form-validation>
+                </x-card>
+
+                <form-readonly></form-readonly>
+
+                <x-card>
+                    @slot('title')
+                        Trix Editor
+                    @endslot
+
+                    <wysiwyg></wysiwyg>
+                </x-card>
             </div>
-
-            <x-card>
-                @slot('title')
-                    Horizontal Form
-                @endslot
-
-                <form-components></form-components>
-            </x-card>
-
-            <x-card>
-                @slot('title')
-                    Form with Built Validation
-                @endslot
-
-                <form-validation></form-validation>
-            </x-card>
-
-            <form-readonly></form-readonly>
-
-            <x-card>
-                @slot('title')
-                    Trix Editor
-                @endslot
-
-                <wysiwyg></wysiwyg>
-            </x-card>
         </div>
     </div>
 </div>
